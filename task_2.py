d70ca6c258921d88871a40f812f43220b060cf20
Denys Tryshkin <denystryshkin@gmail.com>
2026-01-08 21:52:23 -0500

    added task_2

---
+++ task_2.py
@@ -0,0 +1,14 @@
+import random
+
+def get_numbers_ticket(min, max, quantity):
+    if not 1 <= min <= max <= 1000:
+        raise ValueError
+    if not (1 <= quantity <= (max - min + 1)):
+        raise ValueError
+    
+    numbers = random.sample(range(min, max + 1), quantity)
+    return numbers
+
+        
+lottery_numbers = get_numbers_ticket(1, 49, 6)
+print("Ваші лотерейні числа:", lottery_numbers)
